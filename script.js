9fdf4313e6d450ef606b3b720b0ae58476de98ec
minegameing50 <atuldhanotiya7@gmail.com>
2025-08-11 15:17:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function validateLogin(event) {
+    event.preventDefault(); // stop default form submit
+
+    let password = document.getElementById("password").value;
+    let errorMsg = document.getElementById("error-msg");
+
+    if (password.length < 6) {
+        errorMsg.textContent = "Password must be at least 6 characters.";
+        errorMsg.style.color = "red";
+        return false; // stop form submit
+    } else {
+        errorMsg.textContent = "";
+        // Fast redirect
+        window.location.href = "https://minegameing50.github.io/Wellcome-back/";
+        return true;
+    }
+}
+
+function fastRedirect(url) {
+    window.location.href = url;
+}
